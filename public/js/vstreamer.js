c0caf728381541cb60ee8c380d90f8bafe3a8641
Michael Joyce <ubermichael@gmail.com>
2020-08-06 11:31:32 -0700

    buggy. starting over.

--- public/js/vstreamer.js
+++ public/js/vstreamer.js
@@ -2,28 +2,35 @@
 
   function loadJson(data) {
     template = Handlebars.compile($("#screen-template").html());
-    data.forEach(function(d, i){
+    data.forEach(function (d, i) {
       d.image = 'public/screenshot/touched/' + d.image;
       $("#scenes").append(template(d));
     });
-    $(".media").draggable({
+    $(".clip").draggable({
       revert: true,
       zIndex: 100
     });
+
     $("#playlist").droppable({
-      accept: '.media',
+      accept: '.clip',
       activeClass: 'active',
       hoverClass: 'hover',
 
-      drop: function(event, ui) {
+      drop: function (event, ui) {
         let clone = $(ui.draggable).clone();
-        clone.draggable({ destroy: true }).removeClass('ui-draggable-dragging ui-draggable ui-draggable-handle')
-          .attr('style', '');
-
+        clone.draggable({destroy: true})
+        .removeClass('ui-draggable-dragging ui-draggable ui-draggable-handle')
+        .removeClass('clip')
+        .attr('style', '');
         $(this).append(clone);
       }
-    }).sortable().disableSelection();
+    });
   }
+
+  $("#playlist").on('mouseenter', function(e) {
+    console.log("mouse enter");
+    $("#playlist .media").sortable();
+  });
 
   function init() {
     $.getJSON('touched.json', null, loadJson);
