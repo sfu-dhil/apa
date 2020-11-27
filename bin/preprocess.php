f8ae0a91310d3b72a1bc1d3ce94b004a0f725b70
Michael Joyce <ubermichael@gmail.com>
2020-11-27 13:38:01 -0800

    tools to make it go

---
+++ bin/preprocess.php
@@ -0,0 +1,19 @@
+<?php
+
+$filename = $argv[1];
+$base = basename($filename, '.json');
+$imgPath = $argv[2] . '/' . $base;
+
+$data = file_get_contents($argv[1]);
+$json = json_decode($data);
+
+$start = $json[0];
+
+foreach($json as $i => $v) {
+    $v->Video = $start->Video;
+    $v->URL = $start->URL;
+    $v->Image = $imgPath . '/' . $base . '_' . $i . '.png';
+}
+
+$data = json_encode($json);
+echo $data;
